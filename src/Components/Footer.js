e7aab52b075fb3f899a2aaa9f0353493fe3550ec
Hanan <ensw03217@student.su.edu.krd>
2020-12-23 14:50:03 +0300

    color and style of footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,29 +1,12 @@
-import React from "react";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
 
 function Footer() {
   return (
-    <footer 
-    // style={{position: "fixed", bottom: "0",width:"100%"}} //fixed-bottom
-    className={"text-light text-center bg-secondary flex-shrink-1"}
-    >
-      <small style={{ marginRight: "20px" }}>Developed by </small>
-      <a href="https://github.com/7ananDlshad" style={{ color: "white" }}>
-        <FontAwesomeIcon icon={faGithub} />
-        <small style={{ marginRight: "20px" }}>Hanan</small>
-
-      </a>
-      <a href="https://github.com/QaysAyad" style={{ color: "white" }}>
-        <FontAwesomeIcon icon={faGithub} />
-        <small style={{ marginRight: "20px" }}>Qays</small>
-
-      </a>
-      <a href="https://github.com/shirin-muataz" style={{ color: "white" }}>
-        <FontAwesomeIcon icon={faGithub} />
-        <small style={{ marginRight: "20px" }}>Shirin</small>
-
-      </a>
+    <footer className="text-light text-center flex-shrink-1 footer-style">
+      <p className="py-3">
+        Developed with <span className="orange-color">❤</span> by
+        <span className="orange-color"> Persevers</span>
+      </p>
     </footer>
   );
 }
