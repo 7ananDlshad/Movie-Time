2ebb8b82ce535612e9b53c3f0f251505e3d0ecde
Hanan <ensw03217@student.su.edu.krd>
2021-03-11 23:34:05 +0300

    changing logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { Navbar as NavBarBootstarp, Nav } from 'react-bootstrap';
 import SearchBox from './SearchBox';
 import { Link } from 'react-router-dom';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logo from '../images/logo.webp';
 
 export default function Navbar() {
   return (
     <NavBarBootstarp expand="lg" className="flex-shrink-1 navbar-style">
       <Link to="/">
         <NavBarBootstarp.Brand className="white-color">
-          <FontAwesomeIcon
-            icon={faFilm}
-            style={{ fontSize: '25px' }}
-            className="orange-color"
-          />
+          <img src={logo} alt="fav icon" width="30" height="30" />
         </NavBarBootstarp.Brand>
       </Link>
       <NavBarBootstarp.Toggle aria-controls="basic-Navbar-nav" />
