d963e7521abf2b8e1d8e5b2c9a8ea1855a11eaea
Hanan <ensw03217@student.su.edu.krd>
2021-04-19 02:40:59 +0300

    design for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { databaseRef } from '../firebase';
+import logo from '../images/logo.webp';
+
+import {
+  faLinkedin,
+  faTwitterSquare,
+  faGithubSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer() {
   const [feedback, setFeedback] = useState({
@@ -23,39 +31,119 @@ function Footer() {
   };
 
   return (
-    <footer className="text-light text-center flex-shrink-1 footer-style">
-      <p className="py-3">
+    <footer className="text-light footer-style">
+      <Row className="mx-5 my-3">
+        <Col>
+          <h2>Find Us</h2>
+          <div>
+            <a href="https://www.linkedin.com/in/hanandlshad/" target="_blank">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '35px' }}
+                className="white-color"
+              />
+            </a>
+            <a href="https://github.com/7ananDlshad" target="_blank">
+              <FontAwesomeIcon
+                icon={faGithubSquare}
+                style={{ fontSize: '35px' }}
+                className="white-color mx-3"
+              />
+            </a>
+
+            <a href="https://twitter.com/7ananTaha" target="_blank">
+              <FontAwesomeIcon
+                icon={faTwitterSquare}
+                style={{ fontSize: '35px' }}
+                className="white-color"
+              />
+            </a>
+          </div>
+          <h2 className="mt-5">E-mail</h2>
+          <div>
+            <p>info@movie.com</p>
+          </div>
+          {/* <h2>Our Team</h2>
+          <ol>
+            <li className="py-3 size-of-li">
+              Hanan Dlshad&nbsp;&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '25px' }}
+                className="orange-color"
+              />
+            </li>
+
+            <li className="pb-3 size-of-li">
+              Shna Rafeeq&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '25px' }}
+                className="orange-color"
+              />
+            </li>
+            <li className="pb-3 size-of-li">
+              Azhin Rashid&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '25px' }}
+                className="orange-color"
+              />
+            </li>
+            <li className="pb-3 size-of-li">
+              Eman Yusef&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '25px' }}
+                className="orange-color"
+              />
+            </li>
+            <li className="size-of-li">
+              Sara Khalil&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: '25px' }}
+                className="orange-color"
+              />
+            </li>
+          </ol> */}
+        </Col>
+        <Col>
+          <h2 className="mb-3">Contact Us</h2>
+          <Form onSubmit={addFeedback}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Your Email"
+                value={feedback.email}
+                onChange={handleEmail}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={5}
+                placeholder="write Your Message"
+                value={feedback.message}
+                onChange={handleMessage}
+                required
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" style={{ float: 'right' }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <p className="py-3 pl-5">
         Developed with <span className="orange-color">❤</span> by
         <span className="orange-color"> Persevers</span>
       </p>
-      <Form onSubmit={addFeedback}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter Your Email"
-            value={feedback.email}
-            onChange={handleEmail}
-            required
-          />
-        </Form.Group>
-
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            placeholder="write Your Message"
-            value={feedback.message}
-            onChange={handleMessage}
-            required
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
     </footer>
   );
 }
