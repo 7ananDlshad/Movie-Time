d36494ec0b952a499893e3697a531aa1d39c2c84
Hanan <ensw03217@student.su.edu.krd>
2021-05-20 06:59:33 +0300

    connect website to another firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLdrDkCBfKYQHRbZAAuCPAK3t2DvIEaok',
-  authDomain: 'movie-cdf3c.firebaseapp.com',
-  projectId: 'movie-cdf3c',
-  storageBucket: 'movie-cdf3c.appspot.com',
-  messagingSenderId: '489305636668',
-  appId: '1:489305636668:web:13c74274c625c6b2b1e839',
+  apiKey: 'AIzaSyDIiN9PRkzGCfKb0z2cxIR0TRDCN9NZF1E',
+  authDomain: 'movie-49698.firebaseapp.com',
+  projectId: 'movie-49698',
+  storageBucket: 'movie-49698.appspot.com',
+  messagingSenderId: '969410738630',
+  appId: '1:969410738630:web:36834af3f17cf65510dac7',
 };
 
 firebase.initializeApp(firebaseConfig);
